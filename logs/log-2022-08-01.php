0cd4fb804ad9d4e1ba7ea771dafadc042f755acb
hanifroyyanramadhan <hanifroyyanramadhan@gmail.com>
2022-08-03 14:14:36 +0700

    first commit

---
+++ logs/log-2022-08-01.php
@@ -0,0 +1,163 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-08-01 14:36:41 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 14:37:11 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:37:12 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:37:19 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:37:22 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:37:24 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:37:26 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 14:37:31 --> Severity: error --> Exception: Undefined constant "nama_desa" C:\xampp\htdocs\desa\donjo-app\views\gis\content_desa_web.php 54
+ERROR - 2022-08-01 14:38:50 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 14:38:59 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:39:02 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:39:04 --> Severity: error --> Exception: Cannot access offset of type string on string C:\xampp\htdocs\desa\donjo-app\helpers\opensid_helper.php 840
+ERROR - 2022-08-01 14:42:30 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 14:43:24 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 14:43:38 --> Curl error: Operation timed out after 4000 milliseconds with 0 out of 0 bytes received Code: 28
+ERROR - 2022-08-01 14:43:38 --> Array
+(
+    [url] => https://api.github.com/repos/opensid/opensid/releases/latest
+    [content_type] => 
+    [http_code] => 0
+    [header_size] => 0
+    [request_size] => 0
+    [filetime] => -1
+    [ssl_verify_result] => 1
+    [redirect_count] => 0
+    [total_time] => 4.000795
+    [namelookup_time] => 0.002336
+    [connect_time] => 0.793645
+    [pretransfer_time] => 0
+    [size_upload] => 0
+    [size_download] => 0
+    [speed_download] => 0
+    [speed_upload] => 0
+    [download_content_length] => -1
+    [upload_content_length] => -1
+    [starttransfer_time] => 0
+    [redirect_time] => 0
+    [redirect_url] => 
+    [primary_ip] => 20.205.243.168
+    [certinfo] => Array
+        (
+        )
+
+    [primary_port] => 443
+    [local_ip] => 192.168.8.102
+    [local_port] => 50449
+    [http_version] => 0
+    [protocol] => 2
+    [ssl_verifyresult] => 0
+    [scheme] => HTTPS
+    [appconnect_time_us] => 0
+    [connect_time_us] => 793645
+    [namelookup_time_us] => 2336
+    [pretransfer_time_us] => 0
+    [redirect_time_us] => 0
+    [starttransfer_time_us] => 0
+    [total_time_us] => 4000795
+)
+
+ERROR - 2022-08-01 14:43:38 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 14:46:58 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 14:47:05 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 14:47:14 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 14:47:37 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 15:05:03 --> Curl error: Operation timed out after 5002 milliseconds with 0 bytes received Code: 28
+ERROR - 2022-08-01 15:05:03 --> Array
+(
+    [url] => https://api.github.com/repos/opensid/opensid/releases/latest
+    [content_type] => 
+    [http_code] => 0
+    [header_size] => 0
+    [request_size] => 180
+    [filetime] => -1
+    [ssl_verify_result] => 0
+    [redirect_count] => 0
+    [total_time] => 5.002979
+    [namelookup_time] => 0.054716
+    [connect_time] => 0.133437
+    [pretransfer_time] => 2.133844
+    [size_upload] => 0
+    [size_download] => 0
+    [speed_download] => 0
+    [speed_upload] => 0
+    [download_content_length] => -1
+    [upload_content_length] => 0
+    [starttransfer_time] => 0
+    [redirect_time] => 0
+    [redirect_url] => 
+    [primary_ip] => 20.205.243.168
+    [certinfo] => Array
+        (
+        )
+
+    [primary_port] => 443
+    [local_ip] => 192.168.8.102
+    [local_port] => 50537
+    [http_version] => 0
+    [protocol] => 2
+    [ssl_verifyresult] => 0
+    [scheme] => HTTPS
+    [appconnect_time_us] => 2133432
+    [connect_time_us] => 133437
+    [namelookup_time_us] => 54716
+    [pretransfer_time_us] => 2133844
+    [redirect_time_us] => 0
+    [starttransfer_time_us] => 0
+    [total_time_us] => 5002979
+)
+
+ERROR - 2022-08-01 15:05:03 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 15:07:11 --> Curl error: SSL connection timeout Code: 28
+ERROR - 2022-08-01 15:07:11 --> Array
+(
+    [url] => https://api.github.com/repos/opensid/opensid/releases/latest
+    [content_type] => 
+    [http_code] => 0
+    [header_size] => 0
+    [request_size] => 0
+    [filetime] => -1
+    [ssl_verify_result] => 1
+    [redirect_count] => 0
+    [total_time] => 4.013665
+    [namelookup_time] => 2.646927
+    [connect_time] => 3.990347
+    [pretransfer_time] => 0
+    [size_upload] => 0
+    [size_download] => 0
+    [speed_download] => 0
+    [speed_upload] => 0
+    [download_content_length] => -1
+    [upload_content_length] => -1
+    [starttransfer_time] => 0
+    [redirect_time] => 0
+    [redirect_url] => 
+    [primary_ip] => 20.205.243.168
+    [certinfo] => Array
+        (
+        )
+
+    [primary_port] => 443
+    [local_ip] => 192.168.8.102
+    [local_port] => 50567
+    [http_version] => 0
+    [protocol] => 2
+    [ssl_verifyresult] => 0
+    [scheme] => HTTPS
+    [appconnect_time_us] => 0
+    [connect_time_us] => 3990347
+    [namelookup_time_us] => 2646927
+    [pretransfer_time_us] => 0
+    [redirect_time_us] => 0
+    [starttransfer_time_us] => 0
+    [total_time_us] => 4013665
+)
+
+ERROR - 2022-08-01 15:07:11 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 15:35:28 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 15:35:29 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 15:35:38 --> Severity: error --> Exception: Undefined constant "gambar" C:\xampp\htdocs\desa\themes\natra\widgets\arsip_artikel.php 25
+ERROR - 2022-08-01 15:35:46 --> Severity: error --> Exception: count(): Argument #1 ($value) must be of type Countable|array, null given C:\xampp\htdocs\desa\donjo-app\views\nav.php 77
+ERROR - 2022-08-01 11:16:21 --> 404 Page Not Found: Sitemen/index

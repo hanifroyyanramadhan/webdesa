df6a39e57d7961655ba7573bb4ef73a6139c7c79
hanifroyyanramadhan <hanifroyyanramadhan@gmail.com>
2022-08-03 14:37:04 +0700

    first commit

---
+++ logs/log-2022-08-03.php
@@ -0,0 +1,48 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-08-03 14:32:28 --> Curl error: Operation timed out after 5000 milliseconds with 0 bytes received
+ERROR - 2022-08-03 14:32:28 --> Array
+(
+    [url] => https://pantau.opensid.my.id/index.php/api/track/desa?token=da39a3ee5e6b4b0d3255bfef95601890afd80709
+    [content_type] => 
+    [http_code] => 0
+    [header_size] => 0
+    [request_size] => 199
+    [filetime] => -1
+    [ssl_verify_result] => 0
+    [redirect_count] => 0
+    [total_time] => 5.001297
+    [namelookup_time] => 0.08476
+    [connect_time] => 0.128755
+    [pretransfer_time] => 0.36019
+    [size_upload] => 548
+    [size_download] => 0
+    [speed_download] => 0
+    [speed_upload] => 109
+    [download_content_length] => -1
+    [upload_content_length] => 548
+    [starttransfer_time] => 0.360209
+    [redirect_time] => 0
+    [redirect_url] => 
+    [primary_ip] => 103.146.203.49
+    [certinfo] => Array
+        (
+        )
+
+    [primary_port] => 443
+    [local_ip] => 192.168.43.39
+    [local_port] => 55520
+    [http_version] => 0
+    [protocol] => 2
+    [ssl_verifyresult] => 0
+    [scheme] => HTTPS
+    [appconnect_time_us] => 356876
+    [connect_time_us] => 128755
+    [namelookup_time_us] => 84760
+    [pretransfer_time_us] => 360190
+    [redirect_time_us] => 0
+    [starttransfer_time_us] => 360209
+    [total_time_us] => 5001297
+)
+
+ERROR - 2022-08-03 14:36:21 --> Severity: error --> Exception: Call to undefined function setting() C:\xampp\htdocs\desaku\themes\esensi\widgets\peta_wilayah_desa.php 37
